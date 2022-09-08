e60203b4fa8f03c0aa57f396da30d5fc19720943
haydenridd98 <90428727+haydenridd98@users.noreply.github.com>
2022-09-08 16:49:44 -0600

    Update groupGenerator.go

--- groupGenerator.go
+++ groupGenerator.go
@@ -8,23 +8,44 @@ import (
 )
 
 func main() {
-	// open file
-	f, err := os.Open("students.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	// remember to close the file at the end of the program
-	defer f.Close()
 
-	// read the file line by line using scanner
-	scanner := bufio.NewScanner(f)
 
-	for scanner.Scan() {
-		// do something with a line
-		fmt.Printf("line: %s\n", scanner.Text())
-	}
+    // prompt user for number of students in each group
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+    fmt.Println("How many students should be in each group? ")
+    var studNum int 
+
+    // assign input to variable
+    fmt.Scanln(&studNum)
+
+    // test to see if input is int
+    fmt.Print(studNum * 5)
+
+
+    // open file
+    f, err := os.Open("IS405-562 - Section002.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    // close the file
+    defer f.Close()
+
+    // read the file
+    scanner := bufio.NewScanner(f)
+
+	names := []string{}
+
+
+    for scanner.Scan() {
+        // append each line to array
+        names = append(names, scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    // print array
+	fmt.Printf("%v", names)
+
 }
